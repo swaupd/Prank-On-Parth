8291feffa8c7312e036f8e554fad2aa5c0779aa9
Swarit <boricpowderacid@gmail.com>
2025-07-28 16:29:43 +0530

    Changed bat

--- this.bat
+++ this.bat
@@ -1,17 +1,10 @@
-
 @echo off
 
-:: Delete old task if it exists
-schtasks /Delete /TN "PrankParth" /F >nul 2>&1
+REM Delete old task if it exists
+schtasks /Delete /TN "PrankParth" /F
 
-:: Create new task that runs your Python script in the right folder
-schtasks /Create /TN "PrankParth" ^
- /TR "cmd.exe /c \"cd /d C:\Users\manot\Prank-On-Parth && C:\Users\manot\Prank-On-Parth\thisVenv\Scripts\python.exe main.py > prank.log 2>&1\"" ^
- /SC ONCE /ST 00:00 ^
- /RL HIGHEST ^
- /RU "manot" ^
- /RP "ParthIsh@2006" ^
- /F
+REM Create the scheduled task
+schtasks /Create /TN "PrankParth" /TR "cmd /c cd /d C:\Users\manot\Prank-On-Parth && C:\Users\manot\Prank-On-Parth\thisVenv\Scripts\python.exe main.py" /SC ONCE /ST 00:00 /RL HIGHEST /F /RU manot /RP ParthIsh@2006
 
-:: Run the task immediately
+REM Run the task
 schtasks /Run /TN "PrankParth"
